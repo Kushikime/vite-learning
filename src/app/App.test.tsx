e9e6fc258dfd71c59e134e629059a1623df99978
Kushikime <demcsakerik@gmail.com>
2023-07-20 21:54:30 +0200

    Update: added vitest, setup configs. create a basic unit test moved App.tsx to app folder

---
+++ src/app/App.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+
+import App from './App';
+
+describe('App', () => {
+  it('Renders Hello, Vite!', () => {
+    render(<App />);
+
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Hello, Vite!');
+  });
+});
